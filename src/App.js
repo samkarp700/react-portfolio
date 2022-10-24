c2f16318c81496fe7215d54b59e0b7dcc210b66b
Sam Karp <sfcypret@gmail.com>
2022-10-24 18:57:03 -0400

    updated pages, added psuedo code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import './App.css';
+import React from 'react';
+//import about
+//import header - with nav
+//import footer - links
+//import projects/portfolio 
+//import contactform 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1> test </h1>
     </div>
   );
 }
