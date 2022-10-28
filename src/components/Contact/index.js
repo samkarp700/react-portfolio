5ea0cf634bdf32ca7a06a1390b84d7d275f9c377
Sam Karp <sfcypret@gmail.com>
2022-10-27 23:48:33 -0400

    contact form added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers'
 
 function Contact() {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+        if (!errorMessage) {
+            setFormState({ [e.target.name]: e.target.value })
+            console.log('Form', formState);
+        }
+
+    };
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            if (!isValid) {
+                setErrorMessage('Your Email is invalid.');
+            } else {
+                setErrorMessage('')
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+    }
+
     return (
         <section class="bg-light">
-        <form class="h-screen container mx-auto grid gap-4 place-content-center static">
-            <div class="container-md bg-green shadow-lg rounded px-20 py-20">
+            <div class="h-screen flex flex-col items-center justify-center bg-light shadow-lg rounded px-20 py-20">
             <h1 class="italic font-bold text-center">Contact Me!</h1>
+        <form onSubmit={handleSubmit}>
             <div>
-                <label for='name'
-                class="font-medium text-dark mr-4"
+                <label htmlFor='name'
+                class="font-medium text-dark w-full mr-3 py-5 px-4 h-2 rounded mb-2"
                 >Name:</label>
-                <input type="text"/>
+                <input type="text" defaultValue={name} onBlur={handleChange} name="name"/>
             </div>
             <div>
-                <label for='email'
-                class="font-medium text-dark mr-4"
+                <label htmlFor='email'
+                class="font-medium text-dark w-full mr-3 py-5 px-4 h-2 rounded mb-2"
                 >Email:</label>
-                <input type="email"></input>
+                <input type="email" defaultValue={email} onBlur={handleChange} name="email"/>
             </div>
             <div>
-                <label for="message"
-                class="font-medium text-dark mr-4"
+                <label htmlFor="message"
+                class="font-medium text-dark w-full mr-3 py-5 px-4 h-2 rounded mb-2"
                 >Message:</label>
-                <textarea name="message"></textarea>
+                <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5"/>
             </div>
+            {errorMessage && (
+                <div>
+                    <p>{errorMessage}</p>
+                </div>
+            )}
             <button type="submit"
-            class="hover:bg-yellow bg-light relative mt-2 w-full inline-flex items-center justify-center px-6 rounded-full shadow-lg"
+            class="hover:bg-yellow bg-green relative mt-2 w-full inline-flex items-center justify-center px-6 rounded-full shadow-lg"
             >Submit</button>
-            </div>
+            
         </form>
+        </div>
         </section>
+        
     )
 }
 
