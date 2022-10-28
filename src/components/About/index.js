116bebacbf54d47095e113804fdeb02adf47a827
Sam Karp <sfcypret@gmail.com>
2022-10-27 23:28:11 -0400

    contact page added and styled

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,7 +13,7 @@ function About() {
                         <h1 class="italic px-12 md:px-0 md:text-4xl text-2xl font-medium tracking-light md:space-y-2 pl-10 pb-6  md:pl-0">
                             <span class="block">About Me</span>
                         </h1>
-                        <p class='italic leading-relaxed'>
+                        <p class='font-medium italic leading-relaxed'>
                     Welcome to my portfolio. I began coding in the middle of 2022 when I enrolled in UCF's Full Stack Web Engineering Bootcamp. 
                     I currently work as a customer success engineer where I am responsible for configuring and implementing clients into our platform. 
                     My previous experience ranges from teaching to being a customer success manager as well as a self-taught vegan baker on the side. 
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,9 +2,34 @@ import React from 'react';
 
 function Contact() {
     return (
-        <div>
-            <h1>contact test</h1>
-        </div>
+        <section class="bg-light">
+        <form class="h-screen container mx-auto grid gap-4 place-content-center static">
+            <div class="container-md bg-green shadow-lg rounded px-20 py-20">
+            <h1 class="italic font-bold text-center">Contact Me!</h1>
+            <div>
+                <label for='name'
+                class="font-medium text-dark mr-4"
+                >Name:</label>
+                <input type="text"/>
+            </div>
+            <div>
+                <label for='email'
+                class="font-medium text-dark mr-4"
+                >Email:</label>
+                <input type="email"></input>
+            </div>
+            <div>
+                <label for="message"
+                class="font-medium text-dark mr-4"
+                >Message:</label>
+                <textarea name="message"></textarea>
+            </div>
+            <button type="submit"
+            class="hover:bg-yellow bg-light relative mt-2 w-full inline-flex items-center justify-center px-6 rounded-full shadow-lg"
+            >Submit</button>
+            </div>
+        </form>
+        </section>
     )
 }
 
